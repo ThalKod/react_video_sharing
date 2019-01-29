99457140820bce03de329bb88b9b49c9ff0e8a34
ThalKod <tmarcelin@thal.tech>
2019-01-29 09:33:59 -0500

    add basic Header component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import Header from "./Header";
 
 export default class HomePage extends React.Component{
   render(){
     return (
         <div>
-         Hello World !
+          <Header />
         </div>
     )
   }
