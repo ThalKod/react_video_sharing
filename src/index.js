f015da31f59858f45f34f17a532509a2fcce386c
ThalKod <tmarcelin@thal.tech>
2019-01-29 10:22:55 -0500

    ejct and configure CRA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import "./styles/styles.scss";
+import "./styles/styles.less";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
