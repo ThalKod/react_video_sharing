e3ad2abd38f8a9fb2b3382be5a97287c2ef8382d
ThalKod <tmarcelin@thal.tech>
2019-01-29 10:53:37 -0500

    add SearchBar styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import SearchBar from "./SearchBar";
+
 export default class Header extends React.Component{
   render(){
     return (
-        <div>Hello Header !</div>
+        <div>
+          <div className="col-lg-6 col-sm-8 col-xs-12">
+            <SearchBar />
+          </div>
+        </div>
     )
   }
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,11 @@ import React from "react";
 export default class SearchBar extends React.Component {
   render(){
     return(
-        <div>
-
+        <div className="topsearch">
+          <div className="input-group">
+            <span className="input-group-addon" id="sizing-addon2"><i className="fa fa-search"></i></span>
+            <input type="text" className="form-control" placeholder="Search" aria-describedby="sizing-addon2"/>
+          </div>
         </div>
     )
   }
