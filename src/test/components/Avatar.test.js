71f64041d62803c33b57809943b88f68823ee371
ThalKod <tmarcelin@thal.tech>
2019-02-14 12:41:47 -0500

    add Avatar component test

---
+++ src/test/components/Avatar.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+
+import Avatar from "../../components/Header/Avatar";
+
+describe("Avatar", () => {
+  let wrapper;
+  beforeEach(() => {
+    const username = "John Doe";
+    wrapper = shallow(<Avatar username={username}/>);
+  });
+
+  it("should render the Avatar component correctly", () => {
+      expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("should render the correct initials", () => {
+    expect(
+        wrapper.containsMatchingElement(<div>JD</div>)
+    ).toBe(true);
+  });
+});
