1e6eff1c3c7e0f45a526ce36bb4c712e88b0ea1b
ThalKod <tmarcelin@thal.tech>
2019-02-20 09:54:16 -0500

    add UploadPicker content

--- src/components/UploadPicker.js
+++ src/components/UploadPicker.js
@@ -4,7 +4,28 @@ import requireLoggedIn from "./HOC/requireLoggedIn";
 
 const UploadPicker = () => {
     return(
-        <div>Hello Picker</div>
+        <div className="content-wrapper">
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12 upload-page">
+                  <div className="u-area">
+                    <i className="cv cv cvicon-cv-upload-video"/>
+
+                    <p className="u-text1">Select Video files to upload</p>
+                    <p className="u-text2">or drag and drop video files</p>
+
+                    <button type="submit" className="btn btn-primary u-btn">Upload Video</button>
+                  </div>
+
+                  <div className="u-terms">
+                    <p>By submitting your videos to circle, you acknowledge that you agree to circle's <a href="/">Terms
+                      of Service</a> and <a href="/">Community Guidelines</a>.</p>
+                    <p>Please be sure not to violate others' copyright or privacy rights. Learn more</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
     )
 };
 
