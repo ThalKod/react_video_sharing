7d43e8401669d462c106d09b817fc6ec062e57b7
ThalKod <tmarcelin@thal.tech>
2019-02-21 08:17:53 -0500

    add upload route

---
+++ api/routes/upload.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { requireAuth } = require("../middlewares/auth");
+const { saveVideo } = require("../controlers/upload");
+const router = express.Router();
+
+// Upload Routes
+
+// Initially save a video file into document model before transcoding
+router.post("upload/video", requireAuth, saveVideo);
