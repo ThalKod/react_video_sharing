740a0bc60c6758e0cbcd0977a292833b6fdffb82
ThalKod <tmarcelin@thal.tech>
2019-03-19 11:06:50 -0400

    fix VideoSinglePage remount on url change

--- src/components/VideoSinglePage/VideoSingleTab.js
+++ src/components/VideoSinglePage/VideoSingleTab.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"
 
 // import VideoPlaceholder from "../../assets/images/video_placeholder.png";
 import { request, formatSecondForRendering } from "../../utils";
@@ -27,20 +28,20 @@ export default class VideoSingleTab extends React.Component{
   };
 
   render(){
-    const { name, duration, viewCount } = this.props;
+    const { name, duration, viewCount, id } = this.props;
     const { videoImage } = this.state;
 
     return(
         <div className="h-video row">
           <div className="col-lg-6 col-sm-6">
             <div className="v-img">
-              <a href="/"><img src={videoImage} alt=""/></a>
+              <Link to={`/video/${id}`}><img src={videoImage} alt="cover" /></Link>
               <div className="time">{formatSecondForRendering(duration)}</div>
             </div>
           </div>
           <div className="col-lg-6 col-sm-6">
             <div className="v-desc">
-              <a href="/">{name}</a>
+              <Link to={`/video/${id}`}>{name}</Link>
             </div>
             <div className="v-views">{viewCount} Views</div>
           </div>
