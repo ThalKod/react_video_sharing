947a7ef67a5143abfd5eea33f347479b68dfe6a8
ThalKod <tmarcelin@thal.tech>
2019-04-22 08:48:09 -0400

    add reply comment box

--- src/components/VideoSingle/CommentSingle.js
+++ src/components/VideoSingle/CommentSingle.js
@@ -4,19 +4,54 @@ import { Link } from "react-router-dom"
 import Avatar from "components/Header/Avatar";
 import { formatTimestamps } from "utils";
 
-export default function CommentSingle({ text, author: { username, _id }, createdAt }){
-  return(
-      <div className="cl-comment">
-        <div className="cl-avatar">
-          <Link to={`/channel/${_id}`}>
-            <Avatar username={username}/>
-          </Link>
+
+export default class CommentSingle extends React.Component{
+
+  state= {
+    replyBox: false
+  };
+
+  reply = () => {
+    this.setState({ replyBox: true});
+  };
+
+  render(){
+    const { text, author: { username, _id }, createdAt } = this.props;
+    const { replyBox } = this.state;
+
+    return(
+        <div className="cl-comment">
+          <div className="cl-avatar">
+            <Link to={`/channel/${_id}`}>
+              <Avatar username={username}/>
+            </Link>
+          </div>
+          <div className="cl-comment-text">
+            <div className="cl-name-date"><Link to={`/channel/${_id}`}>{username}</Link> . {formatTimestamps(createdAt)}</div>
+            <div className="cl-text">{text}</div>
+            { !replyBox ?
+                <div className="cl-meta">
+                  <button onClick={this.reply} className="reply-button" type="submit">Reply</button>
+                </div>
+                :
+                <div className="reply-comment">
+                  <div className="rc-comment">
+                    <form>
+                      <textarea
+                          rows="3"
+                          placeholder="Enter comment here..."
+                      />
+                      <button type="submit" onClick={this.handleCommentSubmit}>
+                        <i className="cv cvicon-cv-add-comment"/>
+                      </button>
+                    </form>
+                  </div>
+                </div>
+            }
+          </div>
+          <div className="clearfix"/>
         </div>
-        <div className="cl-comment-text">
-          <div className="cl-name-date"><Link to={`/channel/${_id}`}>{username}</Link> . {formatTimestamps(createdAt)}</div>
-          <div className="cl-text">{text}</div>
-        </div>
-        <div className="clearfix"/>
-      </div>
-  )
+    )
+  }
 }
+
